b4baabe6200f143949235327a2397298325227b2
mintoya <hfm598@gmail.com>
2026-04-01 18:04:24 -0500

    tokenizer

--- ast.c
+++ ast.c
@@ -4,9 +4,24 @@
 #include <stddef.h>
 
 typedef struct astNode_inner astNode_inner;
+typedef enum operation {
+  OP_ASSIGNMENT, // a = b
+  OP_SUBTRACT,   // a - b
+  OP_ADD,        // a + b
+  OP_DIVIDE,     // a / b
+  OP_MULTIPLY,   // a * b
+  OP_SUBSCRIPT,  // a.b
+  OP_REFERANCE,  // #a
+  // dereferance
+} operation;
+
 typedef struct astRoot { // file
   msList(astNode_inner) nodes;
 } astRoot;
+typedef struct astText {
+} astText;
+typedef struct astComment { // ... | /* ... */
+} astComment;
 typedef struct astBlock { // {...}
   msList(astNode_inner) nodes;
 } astBlock;
@@ -15,8 +30,16 @@ typedef struct astArg { // (...)
 } astArg;
 typedef struct astExpr { // ?left ?op right
   astNode_inner *left, *right;
-  // operation op; //TODO
+  operation op;
 } astExpr;
+typedef struct astDecl { // qualifiers  name type =  expr
+  struct {
+    astText *qualifiers;
+    astText *name;
+    astText *type;
+  } *left;
+  astExpr *expr;
+} astDecl;
 typedef struct astIf { // if (x) x else x
   astExpr *condition;
   astExpr *ifTrue;
@@ -29,19 +52,23 @@ typedef struct astWhile { // while (x) x
 typedef struct astFunction { // fn (...) ?? {}
   astArg *arguments;
   astBlock *body;
-  astNode_inner *attributes;
+  msList(astNode_inner) attributes;
 } astFunction;
 
 TU_DEFINE(
     (astNode_inner, u8),
     astRoot,
+    astText,
+    astComment,
     astBlock,
     astArg,
     astExpr,
+    astDecl,
     astIf,
     astWhile,
     astFunction,
 );
+
 typedef struct tlocation {
   usize len;
   ptrdiff_t ptr;
